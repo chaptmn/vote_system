525a3cc1a1665dd5454e0f50661b52876ab4bf03
Rp7rf <westhouse51@gmail.com>
2017-06-22 20:20:57 +0900

    ここまで

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
   # devise_for :users, controllers: {
   #   :omniauth_callbacks => "users/omniauth_callbacks",
   # }
-  get 'user/cal', to: 'user#user_cal'  #ログイン後に表示する画面の設定
+ get 'user/cal', to: 'user#user_cal'  #ログイン後に表示する画面の設定
+  # get 'user/cal', to: 'omniauth_callbacks#user_cal'  #ログイン後に表示する画面の設定
+
   root to: 'welcome#index'
 
   devise_for :users
-   resources :contests do
-     post 'vote', to:'contests#vote'
-     resources :products
-   end
+   # resources :contests do
+   #   post 'vote', to:'contests#vote'
+   #   resources :products
+   # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
