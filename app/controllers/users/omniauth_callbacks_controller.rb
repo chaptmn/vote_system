fca9506cd978f547019273e1bf399a1166f55993
Rp7rf <westhouse51@gmail.com>
2017-06-22 18:16:24 +0900

    一応googleと連携できるように

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,17 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success",
+                              :kind => "Google"
+                              
+      redirect_to 'root'
+      # sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to 'root'
+    end
+  end
+#以下省略
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  # devise_for :users, controllers: {
+  #   :omniauth_callbacks => "users/omniauth_callbacks",
+  # }
+  get 'user/cal', to: 'user#user_cal'  #ログイン後に表示する画面の設定
+  root to: 'welcome#index'
+
+  devise_for :users
    resources :contests do
      post 'vote', to:'contests#vote'
      resources :products
