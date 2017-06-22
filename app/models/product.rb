6a67e8ec3bd3ba44a1b0ae2488f8bfaafe0ec121
chaptmn <loutihuji@icloud.com>
2017-06-22 14:36:43 +0200

    イメージアップローダーの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   belongs_to :content
   belongs_to :user
   has_many :votes
+  mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
   has_many :products
   has_many :votes 
-
-  mount_uploader :image, ImageUploader
-
 end
