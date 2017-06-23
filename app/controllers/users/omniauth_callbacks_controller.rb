3795779ded5662f3ebcd73744ac295cdc30d9157
chaptmn <loutihuji@icloud.com>
2017-06-23 07:51:22 +0200

    認証完了

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,10 +3,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_google(request.env['omniauth.auth'])
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      session['uid'] = @user.uid
       sign_in_and_redirect @user, event: :authentication
     else
-      session['uid'] = @user.uid
-      p session['uid']
       redirect_to root_path 
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def user_id
+    User.find_by(uid: session["uid"]).id
+  end
 end
