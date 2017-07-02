03234a308f6ea30dbd7a27515a28ce4b1f8ba719
chaptmn <loutihuji@icloud.com>
2017-07-02 16:17:30 +0900

    ログイン周りのデバッグ (#73)
    * [WIP]ログイン周りのデバッグ
    
    * ログイン周りの修正

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,6 +2,9 @@ module UserHelper
   def login?
      if session["uid"] == nil 
        link_to 'Signin with Google', user_google_oauth2_omniauth_authorize_path 
+     elsif current_user.nil?
+       #debug用本番の時はいらない
+       link_to 'セッション削除', user_logout_path, :method => :delete
      else
        "ユーザー名: " + current_user.name 
      end 
