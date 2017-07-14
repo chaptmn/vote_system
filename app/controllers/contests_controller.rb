ad578b52f820cfd676ff5abfd79d29846f57cd82
Rp7rf <is0248vx@ed.ritsumei.ac.jp>
2017-07-14 23:09:39 +0900

    結果の画面をいい感じに変更 (#109)

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -20,7 +20,7 @@ class ContestsController < ApplicationController
       @vote = 3 - @user.votes.count
     end
     @chart = @products.map{|pro|
-      ["<a href='#{result_contest_product_path(pro.id)}'>#{pro.title}</a> #{pro.votes.count} 票", pro.votes.count]
+      ["#{pro.title} #{pro.votes.count} 票", pro.votes.count]
     }.sort{|a,b| b[1] <=> a[1]}
     p @chart
   end
