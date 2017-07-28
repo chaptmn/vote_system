7feeb79e435ca77f940cc0e69509a36bbad8cb43
Rp7rf <westhouse51@gmail.com>
2017-07-28 17:01:49 +0900

    userにimageの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622083920) do
+ActiveRecord::Schema.define(version: 20170728075410) do
 
   create_table "contests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "admin_id",                 null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170622083920) do
     t.string   "name"
     t.string   "refresh_token"
     t.string   "access_token"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
